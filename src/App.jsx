3ff382bb935f1680b9c7cdd9712f453a0c5cb789
phoenixfire77777 <phoenixfire77777@gmail.com>
2023-06-28 23:36:37 -0400

    added code to show puppyPals and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import {puppyList} from './data.jsx'
+import { useState } from 'react'
 
 import './App.css'
 
-function App() {
-  
+function App() { 
+ 
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log("puppyList: ", puppyList )
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+
+  function handleClick() {
+    // some logic here
+  }
 
   return (
     <>
       <div>
-        
+      {featPupId && (
+        <div class="puppyInfo">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        { 
+          puppies.map((puppy) => {
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+          
+        }
+           
       </div>
       
     </>
